14b9cfb6a7f772d4c7022baeaf68cc4e1cfecf06
c0a23038c1 <c0a23038c1@edo.teu.ac.jp>
2024-05-07 15:07:23 +0900

    練習２

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -158,10 +158,23 @@ def main():
             pg.display.update()
             time.sleep(1)
             return
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
+        if not (beam is None or bomb is None):
+            if beam.rct.colliderect(bomb.rct):  # ビームと爆弾が衝突したら
+                beam = None
+                bomb = None
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         bomb.update(screen)
+        if bomb is not None:
+            bomb.update(screen)
         if beam is not None: # 練習１
             beam.update(screen)
         pg.display.update()
