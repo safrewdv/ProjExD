98da47e91333074011c63ee3db126e98bee0c5e2
c0a23038c1 <c0a23038c1@edo.teu.ac.jp>
2024-05-07 14:40:58 +0900

    練習１：beamクラス

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -115,18 +115,41 @@ class Bomb:
         screen.blit(self.img, self.rct)
 
 
+class Beam:   # 練習１
+    def __init__(self,bird: Bird):
+        self.img = pg.transform.rotozoom(pg.image.load("fig/beam.png"), 0, 2.0)  #　注意
+        self.rct: pg.Rect = self.img.get_rect()
+        self.rct.left = bird.rct.right
+        self.rct.centery = bird.rct.centery
+        self.vx, self.vy = +5, 0
+        
+    def update(self, screen: pg.Surface):
+        """
+        爆弾を速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        if check_bound(self.rct) == (True, True):
+            self.rct.move_ip(self.vx, self.vy)
+            screen.blit(self.img, self.rct)
+
+
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((900, 400))
     bomb = Bomb((255, 0, 0), 10)
+    beam = None # 練習１
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:  # 練習１
+                beam = Beam(bird)
+
         screen.blit(bg_img, [0, 0])
         
         if bird.rct.colliderect(bomb.rct):
@@ -139,6 +162,8 @@ def main():
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         bomb.update(screen)
+        if beam is not None: # 練習１
+            beam.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
